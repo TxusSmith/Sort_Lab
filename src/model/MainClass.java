0d7da2ff50f3e4dc2250c19129447c38a8c81a53
JuanDVera <juandaveru@gmail.com>
2018-08-12 19:22:00 -0500

    added some classes to work

---
+++ src/model/MainClass.java
@@ -0,0 +1,13 @@
+package model;
+
+public class MainClass {
+	
+	public MainClass() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/visual_Interace/MainVisualClass.java
@@ -0,0 +1,13 @@
+package visual_Interace;
+
+public class MainVisualClass {
+	
+	public MainVisualClass() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
