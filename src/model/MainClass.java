fdcadf359f42bc10effa6b69392c06ef1f1e5228
JuanDVera <juandaveru@gmail.com>
2018-08-21 17:26:56 -0500

    random bytes working, implemented 3 sort algorithms for ints

--- src/model/MainClass.java
+++ src/model/MainClass.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainClass {
@@ -11,24 +12,6 @@ public class MainClass {
 	private static byte aleatoryPorcentage;	
 	
 	public MainClass() {
-		
-	}
-	
-	public static int[] randomInts(int size) {
-		
-		int[] array = new int[size];
-		
-		int i = 0;
-		
-		while (i < size) {
-			
-			int x = ThreadLocalRandom.current().nextInt(0, 2147400000);
-			array[i] = x;
-			
-			i++;
-		}
-		
-		return array;
 		
 	}
 	
@@ -94,7 +77,7 @@ public class MainClass {
     // Main function that sorts arr[l..r] using
     // merge()
     
-    public static void sort(int arr[], int l, int r)
+    public static void ascendentMergeSort(int arr[], int l, int r)
     {
         if (l < r)
         {
@@ -102,15 +85,159 @@ public class MainClass {
             int m = (l+r)/2;
  
             // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr , m+1, r);
+            ascendentMergeSort(arr, l, m);
+            ascendentMergeSort(arr , m+1, r);
  
             // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
+    
+    public static void heapify(int arr[], int n, int i)
+    {
+        int largest = i;  // Initialize largest as root
+        int l = 2*i + 1;  // left = 2*i + 1
+        int r = 2*i + 2;  // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+    
+    public static void ascendentHeapSort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+ 
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    
+
+    // A utility function to get maximum value in arr[]
+    public static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+ 
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    public static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+ 
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+ 
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+ 
+        // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+ 
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+ 
+    // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    public static void ascendentRadixSort(int arr[], int n)
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+ 
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+    
+    public static byte[] randomBytes(int size) {
+    	
+    	byte[] array = new byte[size];
+    	
+    	int i = 0;
+    	
+    	while (i < size) {
+    		
+    		byte x = (byte)ThreadLocalRandom.current().nextInt(0, 120);
+    		array[i] = x;
+    		
+    		i++;
+    	}
+    	
+    	return array;
+    	
+    }
+    
+    public static int[] randomInts(int size) {
+    	
+    	int[] array = new int[size];
+    	
+    	int i = 0;
+    	
+    	while (i < size) {
+    		
+    		int x = ThreadLocalRandom.current().nextInt(0, 2147400000);
+    		array[i] = x;
+    		
+    		i++;
+    	}
+    	
+    	return array;
+    	
+    }
+    
+    
+    
 	
 	public static void main(String[] args) throws IOException {
+		
+		// data reader
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -141,24 +268,30 @@ public class MainClass {
 			
 		}
 		
-		if(arraySize.length() > 0 && arraySize.length() <= 2147483647) {
-			int size = Integer.parseInt(arraySize);
-			if(dataTipe == 2) {
-				
-				int array[] = new int[size];
-				
-				if(howItWillBe == 2) {					
-					array = randomInts(size);
-				}	
-				
-				sort(array, 0, size - 1);
-											
-				for (int i : array) {
-					System.out.println(i);
-				}
-			}
-				
-			}
+		// data analysis
+		
+//		if(arraySize.length() > 0 && arraySize.length() <= 2147483647) {
+//			int size = Integer.parseInt(arraySize);
+//			if(dataTipe == 2) {
+//				
+//				int array[] = new int[size];
+//				
+//				if(howItWillBe == 2) {					
+//					array = randomInts(size);
+//				}	
+//				
+//				ascendentMergeSort(array, 0, size - 1);
+//											
+//				for (int i : array) {
+//					System.out.println(i);
+//				}
+//			}
+//				
+//			}
+		
+		for (byte value : randomBytes(5)) {
+			System.out.println(value);
+		}
 		
 	}
 
